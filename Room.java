ca0d42487365ac02d0ce3536694c43dfa87e2d2b
mp-vc <2519989@edu.vaniercollege.qc.ca>
2025-10-31 10:12:33 -0400

    did question 8

--- Room.java
+++ Room.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Class Room - a room in an adventure game.
  *
@@ -15,11 +17,9 @@
 public class Room 
 {
     private String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
-
+    
+    private HashMap<String, Room> exits;
+    
     /**
      * Create a room described "description". Initially, it has no exits. 
      * "description" is something like "a kitchen" or "an open court yard".
@@ -28,6 +28,7 @@ public class Room
     public Room(String description) 
     {
         this.description = description;
+        exits= new HashMap<String,Room>();
     }
 
     /**
@@ -38,20 +39,9 @@ public class Room
      * @param south The south exit.
      * @param west The west exit.
      */
-    public void setExits(Room north, Room east, Room south, Room west) 
+    public void setExits(String direction, Room neighbor) 
     {
-        if(north != null) {
-            northExit = north;
-        }
-        if(east != null) {
-            eastExit = east;
-        }
-        if(south != null) {
-            southExit = south;
-        }
-        if(west != null) {
-            westExit = west;
-        }
+        exits.put(direction,neighbor);
     }
 
     /**
@@ -64,23 +54,7 @@ public class Room
     
     public Room getExit(String direction)
     {
-        if(direction.equals("north"))
-        {
-            return northExit;
-        }
-        if(direction.equals("east"))
-        {
-            return eastExit;
-        }
-        if(direction.equals("south"))
-        {
-            return southExit;
-        }
-        if(direction.equals("west"))
-        {
-            return westExit;
-        }
-        return null;
+        return exits.get(direction);
     }
     /** * Return a description of the room’s exits, 
     * for example, "Exits: north west". 
